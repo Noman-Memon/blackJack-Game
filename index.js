76b0f040c9e2e2936de596300bc2c7406a0f9414
Noman-Memon <96913980+Noman-Memon@users.noreply.github.com>
2022-12-16 15:29:38 +0500

    1.add css 2. make start button functional 3.use of getElementById("e.g: message") or querySelector("e.g #message")

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-let helloEl = document.getElementById("hello")
-let firsCard = Math.floor(Math.random() * 11) + 1;
-let secondCard = Math.floor(Math.random() * 11) + 1;
-let sum = firsCard + secondCard;
-console.log(sum)
-function Hello() {
-    if (sum < 21) {
-        console.log("Do you want to draw a new card? 🙂")
-    }else if (sum === 21) {
-        console.log("Wohoo! You've got Blackjack! 🥳")
-    }
-    else {
-        console.log("You're out of the game! 😭")
-    }
-    helloEl.innerText = sum
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum")
+function startGame() {
+  let firsCard = Math.floor(Math.random() * 11) + 2;
+  let secondCard = Math.floor(Math.random() * 11) + 2;
+  let sum = firsCard + secondCard;
+  console.log(sum)
+
+  if (sum < 21) {
+    message = "Do you want to draw a new card? 🙂";
+  } else if (sum === 21) {
+    message = "Wohoo! You've got Blackjack! 🥳";
+    sumEl.textContent = `Sum: ${sum}`
+  } else {
+    message = "You're out of the game! 😭";
+  }
+  messageEl.textContent= message
+  
 }
+console.log(messageEl)
