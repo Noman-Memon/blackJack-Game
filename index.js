11fbf4d8b4a6983a8d7c2da24d4586d1387183c5
Noman-Memon <96913980+Noman-Memon@users.noreply.github.com>
2022-12-20 01:52:57 +0500

    render cards using array

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ let sumEl = document.getElementById("sum");
 let cardsEl = document.getElementById("cards-el");
 let firsCard = Math.floor(Math.random() * 11) + 2;
 let secondCard = Math.floor(Math.random() * 11) + 2;
+let cards = [firsCard, secondCard];
 let sum = firsCard + secondCard;
+cardsEl.textContent = "Cards:";
 function renderGame() {
   console.log(sum);
   if (sum < 21) {
@@ -11,21 +13,33 @@ function renderGame() {
   } else if (sum === 21) {
     message = "Wohoo! You've got Blackjack! 🥳";
     sumEl.textContent = `Sum: ${sum}`;
-    cardsEl.textContent = "Cards: " + firsCard + " " + secondCard;
+    cardString = "Cards: ";
+    for (let i = 0; i < cards.length; i += 1) {
+      // cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1];
+      cardString += cards[i] + " ";
+    }
+    cardsEl.textContent = cardString;
   } else {
     message = "You're out of the game! 😭";
     sumEl.textContent = `Sum: ${sum}`;
+    cardString = "Cards: ";
+    for (let i = 0; i < cards.length; i += 1) {
+      // cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1];
+      cardString += cards[i] + " ";
+    }
+    cardsEl.textContent = cardString;
   }
   messageEl.textContent = message;
 }
 
 function startGame() {
-  renderGame()
+  renderGame();
 }
 
 function newCard() {
   let newCard = Math.floor(Math.random() * 11) + 2;
   sum += newCard;
-  renderGame()
+  cards.push(newCard);
+  renderGame();
   // console.log("Drawing a new card from the deck!");
 }
