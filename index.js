080949ec8a9adff5977566e5bbe27a15663e22f4
Noman-Memon <96913980+Noman-Memon@users.noreply.github.com>
2022-12-19 12:18:28 +0500

    add newCard functionality

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
-let messageEl = document.getElementById("message-el")
-let sumEl = document.getElementById("sum")
-function startGame() {
-  let firsCard = Math.floor(Math.random() * 11) + 2;
-  let secondCard = Math.floor(Math.random() * 11) + 2;
-  let sum = firsCard + secondCard;
-  console.log(sum)
-
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum");
+let cardsEl = document.getElementById("cards-el");
+let firsCard = Math.floor(Math.random() * 11) + 2;
+let secondCard = Math.floor(Math.random() * 11) + 2;
+let sum = firsCard + secondCard;
+function renderGame() {
+  console.log(sum);
   if (sum < 21) {
     message = "Do you want to draw a new card? 🙂";
   } else if (sum === 21) {
     message = "Wohoo! You've got Blackjack! 🥳";
-    sumEl.textContent = `Sum: ${sum}`
+    sumEl.textContent = `Sum: ${sum}`;
+    cardsEl.textContent = "Cards: " + firsCard + " " + secondCard;
   } else {
     message = "You're out of the game! 😭";
+    sumEl.textContent = `Sum: ${sum}`;
   }
-  messageEl.textContent= message
-  
+  messageEl.textContent = message;
 }
-console.log(messageEl)
+
+function startGame() {
+  renderGame()
+}
+
+function newCard() {
+  let newCard = Math.floor(Math.random() * 11) + 2;
+  sum += newCard;
+  renderGame()
+  // console.log("Drawing a new card from the deck!");
+}
